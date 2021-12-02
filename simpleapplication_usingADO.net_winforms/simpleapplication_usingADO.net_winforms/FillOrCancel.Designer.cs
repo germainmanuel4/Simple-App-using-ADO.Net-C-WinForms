3b12510c4b94a291520708f1efe9dede86d01220
germainmanuel4 <62697182+germainmanuel4@users.noreply.github.com>
2021-12-02 19:30:32 +0800

    Simple App ADO.Net C# WinForms

---
+++ simpleapplication_usingADO.net_winforms/simpleapplication_usingADO.net_winforms/FillOrCancel.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace simpleapplication_usingADO.net_winforms
+{
+    partial class FillOrCancel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrderID = new System.Windows.Forms.TextBox();
+            this.btnFindByOrderID = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFillDate = new System.Windows.Forms.DateTimePicker();
+            this.dvgCustomerOrders = new System.Windows.Forms.DataGridView();
+            this.btnCancelOrder = new System.Windows.Forms.Button();
+            this.btnFillOrder = new System.Windows.Forms.Button();
+            this.btnFinishUpdates = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCustomerOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Order ID:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtOrderID
+            // 
+            this.txtOrderID.Location = new System.Drawing.Point(209, 72);
+            this.txtOrderID.Name = "txtOrderID";
+            this.txtOrderID.Size = new System.Drawing.Size(202, 31);
+            this.txtOrderID.TabIndex = 1;
+            this.txtOrderID.TextChanged += new System.EventHandler(this.txtOrderID_TextChanged);
+            // 
+            // btnFindByOrderID
+            // 
+            this.btnFindByOrderID.Location = new System.Drawing.Point(451, 63);
+            this.btnFindByOrderID.Name = "btnFindByOrderID";
+            this.btnFindByOrderID.Size = new System.Drawing.Size(138, 49);
+            this.btnFindByOrderID.TabIndex = 3;
+            this.btnFindByOrderID.Text = "Find Order";
+            this.btnFindByOrderID.UseVisualStyleBackColor = true;
+            this.btnFindByOrderID.Click += new System.EventHandler(this.btnFindByOrderID_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(356, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "if filling and order, specify filled date";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dtpFillDate
+            // 
+            this.dtpFillDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFillDate.Location = new System.Drawing.Point(451, 173);
+            this.dtpFillDate.Name = "dtpFillDate";
+            this.dtpFillDate.Size = new System.Drawing.Size(187, 31);
+            this.dtpFillDate.TabIndex = 11;
+            this.dtpFillDate.ValueChanged += new System.EventHandler(this.dtpFillDate_ValueChanged);
+            // 
+            // dvgCustomerOrders
+            // 
+            this.dvgCustomerOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgCustomerOrders.Location = new System.Drawing.Point(72, 277);
+            this.dvgCustomerOrders.Name = "dvgCustomerOrders";
+            this.dvgCustomerOrders.ReadOnly = true;
+            this.dvgCustomerOrders.RowHeadersVisible = false;
+            this.dvgCustomerOrders.RowHeadersWidth = 82;
+            this.dvgCustomerOrders.RowTemplate.Height = 33;
+            this.dvgCustomerOrders.Size = new System.Drawing.Size(813, 263);
+            this.dvgCustomerOrders.TabIndex = 12;
+            this.dvgCustomerOrders.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgCustomerOrders_CellContentClick);
+            // 
+            // btnCancelOrder
+            // 
+            this.btnCancelOrder.Location = new System.Drawing.Point(72, 562);
+            this.btnCancelOrder.Name = "btnCancelOrder";
+            this.btnCancelOrder.Size = new System.Drawing.Size(239, 70);
+            this.btnCancelOrder.TabIndex = 13;
+            this.btnCancelOrder.Text = "Cancel Order";
+            this.btnCancelOrder.UseVisualStyleBackColor = true;
+            this.btnCancelOrder.Click += new System.EventHandler(this.btnCancelOrder_Click);
+            // 
+            // btnFillOrder
+            // 
+            this.btnFillOrder.Location = new System.Drawing.Point(357, 562);
+            this.btnFillOrder.Name = "btnFillOrder";
+            this.btnFillOrder.Size = new System.Drawing.Size(232, 73);
+            this.btnFillOrder.TabIndex = 14;
+            this.btnFillOrder.Text = "Fill Order";
+            this.btnFillOrder.UseVisualStyleBackColor = true;
+            this.btnFillOrder.Click += new System.EventHandler(this.btnFillOrder_Click);
+            // 
+            // btnFinishUpdates
+            // 
+            this.btnFinishUpdates.Location = new System.Drawing.Point(657, 565);
+            this.btnFinishUpdates.Name = "btnFinishUpdates";
+            this.btnFinishUpdates.Size = new System.Drawing.Size(228, 67);
+            this.btnFinishUpdates.TabIndex = 15;
+            this.btnFinishUpdates.Text = "Finish";
+            this.btnFinishUpdates.UseVisualStyleBackColor = true;
+            this.btnFinishUpdates.Click += new System.EventHandler(this.btnFinishUpdates_Click);
+            // 
+            // FillOrCancel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 672);
+            this.Controls.Add(this.btnFinishUpdates);
+            this.Controls.Add(this.btnFillOrder);
+            this.Controls.Add(this.btnCancelOrder);
+            this.Controls.Add(this.dvgCustomerOrders);
+            this.Controls.Add(this.dtpFillDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFindByOrderID);
+            this.Controls.Add(this.txtOrderID);
+            this.Controls.Add(this.label1);
+            this.Name = "FillOrCancel";
+            this.Text = "Fill Or Cancel";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCustomerOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrderID;
+        private System.Windows.Forms.Button btnFindByOrderID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFillDate;
+        private System.Windows.Forms.DataGridView dvgCustomerOrders;
+        private System.Windows.Forms.Button btnCancelOrder;
+        private System.Windows.Forms.Button btnFillOrder;
+        private System.Windows.Forms.Button btnFinishUpdates;
+    }
+}
---
+++ simpleapplication_usingADO.net_winforms/simpleapplication_usingADO.net_winforms/Navigation.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace simpleapplication_usingADO.net_winforms
+{
+    partial class Navigation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnGoToFillOrCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add an account";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGoToFillOrCancel
+            // 
+            this.btnGoToFillOrCancel.Location = new System.Drawing.Point(347, 385);
+            this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
+            this.btnGoToFillOrCancel.Size = new System.Drawing.Size(284, 60);
+            this.btnGoToFillOrCancel.TabIndex = 1;
+            this.btnGoToFillOrCancel.Text = "Fill or cancel an order";
+            this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+            this.btnGoToFillOrCancel.Click += new System.EventHandler(this.btnGoToFillOrCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(807, 525);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(204, 64);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(517, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Manage Customer Order";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 639);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGoToFillOrCancel);
+            this.Controls.Add(this.button1);
+            this.Name = "Navigation";
+            this.Text = "Welcome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGoToFillOrCancel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
